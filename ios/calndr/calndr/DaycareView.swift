8346c88384a4483f46c85ae614c7c824e815bfe3
Jeff Levensailor <jlevensailor@presidio.com>
2025-07-13 11:58:39 -0400

    Reduce ZIP code input field width to appropriate size for 5 digits

--- ios/calndr/calndr/DaycareView.swift
+++ ios/calndr/calndr/DaycareView.swift
@@ -387,6 +387,7 @@ struct DaycareSearchView: View {
                                 themeManager: themeManager
                             )
                             .keyboardType(.numberPad)
+                            .frame(maxWidth: 120)
                             
                             Button("Search") {
                                 searchDaycares()
